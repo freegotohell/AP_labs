53c3ce94a7306016b66ff64fb92ff0b1b4db4d59
lina <freetogo666@outlook.com>
2024-10-18 12:51:19 +0400

    Reapply "added class crawler that download images & create annotation"
    This reverts commit 805f4cfc0abeede1ce6ea3e2393fbf076c0f17ba.

--- main.py
+++ main.py
@@ -14,5 +14,36 @@ def parse() -> tuple[str, str, str]:
     return args.keyword, args.directory, args.annotation
 
 
+class Crawler:
+    def __init__(self, keyword: str, directory: str, limit: int) -> None:
+        self.keyword = keyword
+        self.directory = directory
+        self.limit = limit
+        self.crawler = GoogleImageCrawler(
+            feeder_threads=1,
+            parser_threads=2,
+            downloader_threads=4,
+            storage={'root_dir': directory}
+        )
+
+    def download(self) -> None:
+        self.crawler.crawl(keyword=self.keyword, max_num=self.limit)
+
+    def annotation(self, directory: str) -> str:
+        with open('annotation.cvs', mode='a', newline='', encoding='utf-8') as file:
+            writer = csv.writer(file)
+            writer.writerows(["absolute path", "relative path"])
+            pics = os.listdir(directory)
+            for i in pics:
+                d = os.path.abspath(os.path.join(self.directory, i))
+                f = os.path.relpath(os.path.join(self.directory, i), start=".")
+                writer.writerows([d, f])
+            return file.name
+
+
 def main() -> None:
     keyword, directory, annotation = parse()
+
+
+if __name__ == "__main__":
+    main()
